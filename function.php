f20fd82e040eac9550e57569a2b639ca18c8dd5b
rezaaaa <rsabet1984@gmail.com>
2015-04-15 15:19:00 +0800

    Update function.php

--- function.php
+++ function.php
@@ -1,18 +1,20 @@
 <?php
-	
 	mysql_connect('localhost', 'root', '');
 	mysql_select_db('blog');
 	
-		function addMessage($subject, $message){
-			mysql_query("INSERT INTO messageTable VALUES('', '".$subject."', '".$message."')");
-		}
+	function addMessage($subject, $message)
+	{
+		mysql_query("INSERT INTO messageTable VALUES('', '".$subject."', '".$message."')");
+	}
 		
-		function viewAll(){
-			$query = mysql_query("SELECT * FROM messageTable");
-			while($post = mysql_fetch_array($query)){
-				echo "Subject: ".$post[1]."<br>";
-				echo "Message: ".$post[2]."<br><br>";
-			}	
-		}
+	function viewAll()
+	{
+		$query = mysql_query("SELECT * FROM messageTable");
+		while($post = mysql_fetch_array($query))
+		{
+			echo "Subject: ".$post[1]."<br>";
+			echo "Message: ".$post[2]."<br><br>";
+		}	
+	}
 	
 ?>
