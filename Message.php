d23f4e294811e87d1be80bd4522c3218bc09bbd4
FJRTuquib <FJRTuquib@gmail.com>
2015-04-20 15:47:45 +0800

    Update Message.php

--- Message.php
+++ Message.php
@@ -1,23 +1,29 @@
-<?php include('DAL.php');?>
-<?php session_start(); ?>
+<?php include('DAL.php');
+session_start();
+
+$user_id = $_SESSION['id'];
+$user_type = $_SESSION['type'];?>
 
 <form action="Authentication.php" method="get">
 	<input type="submit" value="LOGOUT">
+	
+	<?php if ($user_type == "admin"){
+	echo "ADMIN"; 
+	}?>
+	
 </form>
+
+
 
 <form action="Message.php" method="post">
 	<b>Subject: <input type="text" name="subject" required><br><br>
-	<b>Messages:<br><textarea cols="30" rows="5" name="message" required></textarea><br><br>
+	<b>Messages:<br><textarea cols="30" rows="5" name="message" required></textarea><br>
 	<input type="submit" name="submit" value="SEND">
 </form>
 
 <br><br><br>
 
 <?php
-
-	$user_id = $_SESSION['id'];
-	$user_type = $_SESSION['type'];
-
 	if(isset($_POST['submit'])){	
 		$subject = $_POST['subject'];
 		$message = $_POST['message'];
@@ -33,20 +39,21 @@
 		
 ?> <br> <?php
 
-if(($user_type == "regular" && $user_id == $output['userID'])  ||  ($user_type == "admin")){?>
+	if(($user_type == "regular" && $user_id == $output['userID'])  ||  ($user_type == "admin")){?>
 	
-<form action="update_confirm.php" method="post">
-	<input type="hidden" name="update_id" value="<?php echo $output["messageID"]; ?>">
-	<input type="hidden" name="update_sub" value="<?php echo $output["subject"]; ?>">
-	<input type="hidden" name="update_mes" value="<?php echo $output["message"]; ?>">
-	<input type="submit" value="EDIT">
-</form>
+		<form action="UD_process.php" method="post">
+			<input type="hidden" name="update_sub" value="<?php echo $output["subject"]; ?>">
+			<input type="hidden" name="update_mes" value="<?php echo $output["message"]; ?>">
+			<input type="submit" name="update_submit" value="EDIT">
+	
+			<input type="hidden" name="delete_id" value="<?php echo $output["messageID"]; ?>">
+			<input type="submit" name="delete_submit" value="DELETE">
+		</form>
 			
-<form action="delete_confirm.php" method="post">
-	<input type="hidden" name="delete_id" value="<?php echo $output["messageID"]; ?>">
-	<input type="submit" value="DELETE">
-</form>
-	
-<?php }else{ ?>
+<?php 
+		echo "____________________________________________________________________"."<br>";
+	}else{ 
+		echo "____________________________________________________________________";
+?>
 <br>
 <?php } } ?>
