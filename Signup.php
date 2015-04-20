96bf814dad2760fc27d73cb24a30315cfe969811
rjom80 <rjom80@users.noreply.github.com>
2015-04-20 16:51:47 +0800

    Create Signup.php

---
+++ Signup.php
@@ -0,0 +1,23 @@
+<html>
+	<title>Sign Up Here!</title>
+	<head></head>
+	<body>
+		<p>Create Account as Admin or Regular user: <br></p>
+		<form method="post">
+			Username: <input type="text" name="username" size="15" required>
+			Password: <input type="password" name="password" size="15" required><br>
+			You want to be: Admin <input type="radio" name="type" value="admin" required>
+			Regular <input type="radio" name="type" value="regular" required><br>
+			<input type="submit" name="signup" value="Register Me!">
+		
+			<?php
+				require_once('RPineroDALSignupUser.php');
+		
+				if(isset($_POST['signup'])){
+					$UserAdd = new UserAdd($_POST['username'], $_POST['password'], $_POST['type']);
+					$UserAdd->Signup(); 
+				}
+			?>
+		</form>
+	</body>
+</html>
