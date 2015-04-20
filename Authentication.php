590f148a6595a87cdd3239a2cec8d0eb83b12206
Jeefcarl <JCTrumatalancera@gmail.com>
2015-04-20 15:01:32 +0930

    Update Authentication.php

--- Authentication.php
+++ Authentication.php
@@ -4,37 +4,39 @@
 	Username: <input type="text" name="user" required><br><br>
 	Password: <input type="password" name="pass" required><br><br>
 	<input type="submit" name="login" value="Submit"><br><br>
-	
+
 	<?php
 		require_once('DAL.php');
 		
 		if(isset($_POST['login'])){
-			$check = new check($_POST['user'], $_POST['pass']);
-			$type = $check->checking();
+			$check = new UserDAL($_POST['user'], $_POST['pass']);
+			$type = $check->AuthenticateUser();
 			$strArray = explode('/',$type);
-			if($strArray[0] == null){
+			if($strArray[3] == null){
 				$t = "null";
 			} else {
-				$t = $strArray[0];
+				$t = $strArray[3];
 			}
-			if($strArray[1] == 0){
+			if($strArray[0] == 0){
 				$i = 0;
 			} else {
-				$i = $strArray[1];
+				$i = $strArray[0];
 			}
 
-			if($t == "admin" || $t =="regular"){
+			if($t == "admin" || $t == "regular"){
 				//This will make admin link to messages;
+				echo "<br>You are an admin";
 				session_start();
 				$_SESSION['id'] = $i;
 				$_SESSION['type'] = $t;
 				header('Location: check.php');
+				echo "Id: {$i}<br>Type{$t}";
 				
 			} else { 
 				echo "<br>Incorrect login Credentials..";
 			} 
+			echo $type;
 		}	
-		
 	?>
 </form>
 
