9bedb9878956ede1625ba9d89bd8dab4c386a58d
FJRTuquib <FJRTuquib@gmail.com>
2015-04-20 15:46:51 +0800

    Create UD_process.php

---
+++ UD_process.php
@@ -0,0 +1,37 @@
+<?php include('DAL.php');?>
+<?php
+
+	if(isset($_POST['update_submit'])) {
+		$update_sub = $_POST['update_sub'];
+		$update_mes = $_POST['update_mes'];
+?>
+
+<form action="update_confirm.php" method="post">
+	<b>Edit Subject: <input type="text" name="edit_subject" value="<?php echo $update_sub; ?>" required><br><br>
+	<b>Edit Messages:<br><textarea cols="50" rows="5" name="edit_message" required><?php echo $update_mes; ?></textarea><br>
+	<input type="hidden" name="edit_id" value="<?php echo $update_id; ?>">
+	<input type="submit" name="edit_submit" value="EDIT">
+</form>
+
+<form action="Message.php" method="get">
+	<input type="submit" value="BACK">
+</form>
+
+<?php
+	}
+	if(isset($_POST['edit_submit'])){	
+		$id = $_POST['edit_id'];
+		$subject = $_POST['edit_subject'];
+		$message = $_POST['edit_message'];
+		$mess = new Message(null, null, null);
+		$mess->updateMessage($subject, $message, $id);
+		header('Location: Message.php');
+	}
+	
+	if(isset($_POST['delete_submit'])) {
+		$delete_id = mysql_real_escape_string($_POST['delete_id']);
+		$mess = new Message(null, null, null);
+		$mess->deleteMessage($delete_id);
+		header('Location: Message.php');
+	}
+?>
