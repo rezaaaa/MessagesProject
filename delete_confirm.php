5ae20ac68c006c4258af07007ab7a5c81787d38d
FJRTuquib <FJRTuquib@gmail.com>
2015-04-20 15:46:17 +0800

    Delete delete_confirm.php

--- delete_confirm.php
+++
@@ -1,9 +0,0 @@
-<?php include('DAL.php');?>
-<?php
-	if(isset($_POST['delete_id'])) {
-		$delete_id = mysql_real_escape_string($_POST['delete_id']);
-		$mess = new Message(null, null, null);
-		$mess->deleteMessage($delete_id);
-		header('Location: Message.php');
-	}
-?>
