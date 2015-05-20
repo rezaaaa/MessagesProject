ef54795b2ddfb4ccecb25a4de1cf0615010f5274
rjom80 <rjom80@users.noreply.github.com>
2015-05-20 17:36:35 +0800

    Create subscribed.blade.php

---
+++ subscribed.blade.php
@@ -0,0 +1,32 @@
+@extends('reddit')
+
+@section('content')
+    <div>
+        <ul>
+            <li>Are you nuts? The Judeo-Christian God is an unfalsifiable hypothesis and so as, Zeus, Thor, Saturn, or any God that has ever mentioned throughout our history.</li>
+            <li>Long ago, people believed in Gods and Goddesses because they were curious about reality.</li>
+            <li>In their time, they'd always frequently asked themselves;</li>
+            <ul>
+                <li>"What's the meaning of life?",</li>
+                <li>"Why are we here?",</li>
+                <li>"Is there reason to live?"</li>
+                <li>"What is my existence worth?",</li>
+                <li>"Is there someone who controls our fate?",</li>
+            </ul>
+            </li>
+            <li>Thinking about it, we were curious yet ignorant about reality. Therefore we became apathetic and created religion.</li>
+            <li>It is sad that people rejoice in false promises and delusional aspirations.</li>
+            <li>Rather than discovering new ideas in such short existence in reality</li>
+            <li>That's why science bitches, it works!</li>
+            <li>The Scientific Method is the most robust process of examining reality, and it will never fail you until the end.</li>
+            <li>Things that was unexplained in the past, can now be interpreted by using the Scientific Method.</li>
+            <li>Now think about things that can't be explained today (except for unfalsifiable entities and hypothesis, so called "pseudo-science"), then imagine what will be it's advent in the future by the use of Scientific Method.</li>
+            <li>Science today changed the world, either good or bad, it connects us humans pertaining to the reality.</li>
+            <li>PS: Death gives you purpose to pursue more in the urgency of Life.</li>
+            <li>Humans are fragile creatures that are curious about their existence in the cosmos.</li>
+            <li>That curiosity made us discover, learn and innovate our world.</li>
+            <li>Therefore, we must part take to a fulfilling future of our civilization...</li>
+            <li>HAIL HYDRA!!! :p</li>
+        </ul>
+    </div>
+@endsection
